22eefb49d91fab63334e18c15af7c1231c7a43ff
0x002500 <tiger_pan@outlook.com>
2024-03-02 11:51:51 +0800

    [fix] fix the problems in the register commands

--- src/main/java/com/zerox/authmod/AuthMod.java
+++ src/main/java/com/zerox/authmod/AuthMod.java
@@ -7,29 +7,24 @@ import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.text.LiteralText;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import static com.mojang.brigadier.arguments.StringArgumentType.getString;
 import static com.mojang.brigadier.arguments.StringArgumentType.word;
 import static net.minecraft.server.command.CommandManager.literal;
+
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.*;
+
 import com.zerox.authmod.command.LoginCommand;
 
 public class AuthMod implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("AuthMod");
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("Thank you for using AuthMod");
-		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> LoginCommand.register(dispatcher);
-				/*
-				dispatcher.register(literal("foo")
-				.executes(context -> {
-					// 对于 1.19 之前的版本，把“Text.literal”替换为“new LiteralText”。
-					// 对于 1.20 之前的版本，请移除“() ->”。
-					context.getSource().sendFeedback(new LiteralText("调用 /foo，不带参数"), false);
-
-					return 1;
-				})));
-				 */
-	}
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Thank you for using AuthMod");
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+            LoginCommand.register(dispatcher);
+        });
+    }
 }
--- src/main/java/com/zerox/authmod/command/LoginCommand.java
+++ src/main/java/com/zerox/authmod/command/LoginCommand.java
@@ -1,29 +1,24 @@
 package com.zerox.authmod.command;
 
+import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.tree.LiteralCommandNode;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.util.Formatting;
 
 import static com.mojang.brigadier.arguments.StringArgumentType.getString;
 import static com.mojang.brigadier.arguments.StringArgumentType.greedyString;
-import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Text;
-import static net.minecraft.server.command.CommandManager.*;
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
+
 
 public class LoginCommand {
-    public static void register(CommandDispatcher<ServerCommandSource> dispatcher){
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(literal("login")
-                //.requires(source -> source.hasPermissionLevel(2)) // 只有管理员能够执行命令。命令不会对非操作员或低于 1 级权限的玩家显示在 tab-完成中，也不会让他们执行。
-                .then(argument("username", greedyString())
-                        .then(argument("password", greedyString())
-                                .executes(ctx -> verify_password(getString(ctx, "username"), getString(ctx, "password")))))); // 你可以在这里处理参数，并处理成命令。
+                .then(argument("password", greedyString())
+                        .executes(ctx -> verify_password(ctx.getSource(), getString(ctx, "password")))));
     }
 
-    public static int verify_password(String username, String password) {
-        final Text text = Text.literal(message).formatted(formatting);
-
-        source.getMinecraftServer().getPlayerManager().broadcastChatMessage(text, MessageType.CHAT, source.getPlayer().getUuid());
+    private static int verify_password(ServerCommandSource source, String password) {
+        // get
         return Command.SINGLE_SUCCESS; // 成功
     }
-}
 }
